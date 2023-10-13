3996c330dd7e53f502f77c93fd08a5bb421411e2
arturo <artmcel@hotmail.com>
2023-10-12 18:39:38 -0600

    add select query

--- app/Http/Controllers/UserCOntroller.php
+++ app/Http/Controllers/UserCOntroller.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserCOntroller extends Controller
 {
     //
 
     public function getAllUsers(){
+
+        $users = DB::select('SELECT name, domicilio FROM users, user_domicilio WHERE users.id = user_domicilio.user_id');
+        return response()->json($users, 200);
         
     }
 }

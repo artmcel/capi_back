36b0732608ad543eb678175cb8c59ecce3d443bf
arturo <artmcel@hotmail.com>
2023-10-12 18:17:47 -0600

    add definition on factory

---
+++ database/factories/UsersDomicilioFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UsersDomicilioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'domicilio' => $this->faker->address(),
+            'numero_exterior' => $this->faker->streetAddress(),
+            'colonia' => $this->faker->city(),
+            'cp' => $this->faker->postcode(),
+            'ciudad' => $this->faker->city()
+        ];
+    }
+}
